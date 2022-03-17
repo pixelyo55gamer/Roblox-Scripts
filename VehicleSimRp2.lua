5817629cb95a23c4f086d550b3d16d760303edd7
Pezjamin <65027106+pixelyo55gamer@users.noreply.github.com>
2022-03-17 20:37:54 +0700

    Create VehicleSimRp2.lua

--- VehicleSimRp2.lua
+++ VehicleSimRp2.lua
@@ -25,33 +25,69 @@ game:GetService("ReplicatedStorage").DealershipSystem.purchase_item:InvokeServer
 if game.Players.LocalPlayer.Character.Humanoid.Died then
     wait(15)
     local plr = game.Players.LocalPlayer
-    plr.Character.HumanoidRootPart.CFrame = CFrame.new(6783.56592, 217.855057, -3679.55835)
+    local foriv = {1,2,3,4,5,6,7,8,9,10}
+    for i,v in pairs(foriv) do
+      wait(0.1)
+      plr.Character.HumanoidRootPart.CFrame = CFrame.new(6783.56592, 217.855057, -3679.55835)
+    end
     plr.Character.Humanoid.WalkToPoint = Vector3.new(6741.62, 217.655, -3656.6)
     wait(6)
-    plr.Character.HumanoidRootPart.CFrame = CFrame.new(6045.66748, 219.757523, -335.621735)
+    local foriv = {1,2,3,4,5,6,7,8,9,10}
+    for i,v in pairs(foriv) do
+      wait(0.1)
+      plr.Character.HumanoidRootPart.CFrame = CFrame.new(6045.66748, 219.757523, -335.621735)
+    end
     plr.Character.Humanoid.WalkToPoint = Vector3.new(6004.92822, 218.749985, -336.673126)
     wait(6)
-    plr.Character.HumanoidRootPart.CFrame = CFrame.new(3831.09155, 219.460663, 848.387878)
+    local foriv = {1,2,3,4,5,6,7,8,9,10}
+    for i,v in pairs(foriv) do
+      wait(0.1)
+      plr.Character.HumanoidRootPart.CFrame = CFrame.new(3831.09155, 219.460663, 848.387878)
+    end
     plr.Character.Humanoid.WalkToPoint = Vector3.new(3824.10571, 218.453125, 821.092041)
     wait(6)
+    local foriv = {1,2,3,4,5,6,7,8,9,10}
+    for i,v in pairs(foriv) do
+      wait(0.1)
     plr.Character.HumanoidRootPart.CFrame = CFrame.new(-241.262787, 225.179489, -1632.3479)
+    end
     plr.Character.Humanoid.WalkToPoint = Vector3.new(-253.263733, 228.234619, -1671.28271)
     wait(6)
-    plr.Character.HumanoidRootPart.CFrame = CFrame.new(568.620667, 310.02652, -2288.83691)
+    local foriv = {1,2,3,4,5,6,7,8,9,10}
+    for i,v in pairs(foriv) do
+      wait(0.1)
+      plr.Character.HumanoidRootPart.CFrame = CFrame.new(568.620667, 310.02652, -2288.83691)
+    end
     plr.Character.Humanoid.WalkToPoint = Vector3.new(540.139648, 307.847229, -2304.3772)
     wait(6)
     plr.Character.HumanoidRootPart.CFrame = CFrame.new(-1492.45142, 394.634125, -2900.95825)
     wait(3)
-    plr.Character.HumanoidRootPart.CFrame = CFrame.new(-1464.39893, 363.584869, -2892.56274)
+    local foriv = {1,2,3,4,5,6,7,8,9,10}
+    for i,v in pairs(foriv) do
+      wait(0.1)
+      plr.Character.HumanoidRootPart.CFrame = CFrame.new(-1464.39893, 363.584869, -2892.56274)
+    end
     plr.Character.Humanoid.WalkToPoint = Vector3.new(-1465.51123, 362.517273, -2900.20825)
     wait(3)
-    plr.Character.HumanoidRootPart.CFrame = CFrame.new(1535.75537, 219.557571, -4547.67822)
+    local foriv = {1,2,3,4,5,6,7,8,9,10}
+    for i,v in pairs(foriv) do
+      wait(0.1)
+      plr.Character.HumanoidRootPart.CFrame = CFrame.new(1535.75537, 219.557571, -4547.67822)
+    end
     plr.Character.Humanoid.WalkToPoint = Vector3.new(1534.08472, 219.217499, -4575.31836)
     wait(6)
-    plr.Character.HumanoidRootPart.CFrame = CFrame.new(3223.19653, 219.157562, -2123.66724)
+    local foriv = {1,2,3,4,5,6,7,8,9,10}
+    for i,v in pairs(foriv) do
+      wait(0.1)
+      plr.Character.HumanoidRootPart.CFrame = CFrame.new(3223.19653, 219.157562, -2123.66724)
+    end
     plr.Character.Humanoid.WalkToPoint = Vector3.new(3239.98535, 217.328094, -2102.21973)
     wait(6)
-    plr.Character.HumanoidRootPart.CFrame = CFrame.new(-205.210861, 391.140137, -3737.43604)
+    local foriv = {1,2,3,4,5,6,7,8,9,10}
+    for i,v in pairs(foriv) do
+      wait(0.1)
+      plr.Character.HumanoidRootPart.CFrame = CFrame.new(-205.210861, 391.140137, -3737.43604)
+    end
     wait(1)
     fireclickdetector(game:GetService("Workspace").VendingMachines.Machines.VendingMachineLab.Buttons.b1.ClickDetector)
     wait(1)
