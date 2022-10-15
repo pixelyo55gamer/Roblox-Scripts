ab79b1e19c275b798a77740b0044395765163be9
Pezjamin <65027106+pixelyo55gamer@users.noreply.github.com>
2022-10-15 21:27:25 +0700

    Update RP2.lua

--- RP2.lua
+++ RP2.lua
@@ -6,7 +6,7 @@ elseif game.PlaceId == 5661005779 then --Piggy Book 2
     --https://www.roblox.com/games/4623386862?privateServerLinkCode=43881783873356048913116130569902
     repeat wait() until game:IsLoaded()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/CMD-X/CMD-X/master/Source", true))()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/pixelyo55gamer/script/main/PiggyRP2.lua", true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/pixelyo55gamer/Roblox-Scripts/main/PiggyRP2.lua", true))()
 elseif game.PlaceId == 734159876 then --Sharkbite
     --https://www.roblox.com/games/734159876?privateServerLinkCode=14794222261867326163537089403627
     for i,v in pairs(game:GetService("Workspace").SonarBuoys.Buoys:GetDescendants()) do
@@ -42,22 +42,23 @@ elseif game.PlaceId == 3233893879 then --Bad Business
         end
 elseif game.PlaceId == 1537690962 then --Bee Swarm Simulator
     repeat wait() until game:IsLoaded()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/pixelyo55gamer/script/main/beeswarmrp2.lua", true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/pixelyo55gamer/Roblox-Scripts/main/beeswarmrp2.lua", true))()
 elseif game.PlaceId == 447452406 then --Robloxian Highschool
     repeat wait() until game:IsLoaded()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/pixelyo55gamer/script/main/RHS.lua", true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/pixelyo55gamer/Roblox-Scripts/main/RHS.lua", true))()
     wait(0.5)
     loadstring(game:HttpGet("https://raw.githubusercontent.com/CMD-X/CMD-X/master/Source", true))()
 elseif game.PlaceId == 5913871729 then --Bad Business Fight
     repeat wait() until game:IsLoaded()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/pixelyo55gamer/script/main/Bad_Business_Script.lua", true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/pixelyo55gamer/Roblox-Scripts/main/Bad_Business_Script.lua", true))()
 elseif game.PlaceId == 2414851778 then --Dungeon Quest
     repeat wait() until game:IsLoaded()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/pixelyo55gamer/script/main/DungeonQuestRp2.lua", true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/pixelyo55gamer/Roblox-Scripts/main/DungeonQuestRp2.lua", true))()
 elseif game.PlaceId == 171391948 then --Vehicle Simulator 2
     repeat wait() until game:IsLoaded()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/pixelyo55gamer/script/main/VehicleSimRp2.lua", true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/pixelyo55gamer/Roblox-Scripts/main/VehicleSimRp2.lua", true))()
     wait(1)
     loadstring(game:HttpGet("https://raw.githubusercontent.com/CMD-X/CMD-X/master/Source", true))()
 elseif game.PlaceId == 6028508797 then --Sharkbite Fight
     loadstring(game:HttpGet("https://raw.githubusercontent.com/CMD-X/CMD-X/master/Source", true))()
+end
