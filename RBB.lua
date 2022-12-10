e11925915fe90ea2f7187e4db8918dfa8ddb2c86
Pezjamin <65027106+pixelyo55gamer@users.noreply.github.com>
2022-12-10 21:40:02 +0700

    Create RBB.lua

---
+++ RBB.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 5036207802 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/78n/Amity/main/RBBattles.lua"))()
+elseif game.PlaceId == 6447798030 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/78n/Amity/main/RBBattlesHub.lua"))()
+end
